f9b7da9fc6eea8ccdae01a80a3995b7e72eedb4e
stvn383 <81706388+stvn383@users.noreply.github.com>
2025-02-20 10:58:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
 
 # Write directly to the app
 st.title("Smoothie Maker 1000 :cup_with_straw:")
