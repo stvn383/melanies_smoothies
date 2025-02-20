f6b1c74032639be1d9678b5aa6f3677c2d66b332
stvn383 <81706388+stvn383@users.noreply.github.com>
2025-02-20 12:24:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """')"""
     st.write(my_insert_stmt)
-    st.stop()
+    #st.stop()
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
